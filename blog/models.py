3ad4c4a3d2b13401355f91989aa3bfb485c858a4
Henry McCormack <hmccormak@protonmail.com>
2024-09-17 12:01:07 -0400

    posts now have their own pages

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
+    link_desc = models.CharField(max_length=500, default="Read more...")
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
